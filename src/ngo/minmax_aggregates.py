fa5ab716a70d5513ff43ca83487f548603a69890
Max Ostrowski <max.ostrowski@potassco.com>
2023-06-15 15:42:38 +0200

    refactor

--- src/ngo/minmax_aggregates.py
+++ src/ngo/minmax_aggregates.py
@@ -127,19 +127,20 @@ class MinMaxAggregator:
             else:
                 lits_without_vars.append(blit)
 
-        # TODO: create a new domain if several conditions are used, also create next_ for this domain
+        # TODO: create a new domain if several conditions are used, also create next_ for this domain, issue #9
         if len(elem.condition) > 1:
-            return [rule]
+            return [rule] # nocoverage
         # TODO: lift restrictions, currently only needed to get some domain
+        # NOTE: this could also be Comparisons, see issue #9
         if elem.condition[0].ast_type != ASTType.Literal or elem.condition[0].atom.ast_type != ASTType.SymbolicAtom:
-            return [rule]
+            return [rule] # nocoverage
 
         condition_pred = (
             elem.condition[0].atom.symbol.name,
             len(elem.condition[0].atom.symbol.arguments),
         )
         if not self.domain_predicates.has_domain(condition_pred) or self.domain_predicates.is_static(condition_pred):
-            return [rule]
+            return [rule] # NOTE: issue #9, this check needs to be done for all conditions
 
         number_of_aggregate = 0
         number_of_element = 0
@@ -156,7 +157,8 @@ class MinMaxAggregator:
         head = SymbolicAtom(Function(LOC, new_name, [weight], False))
         self.domain_predicates.add_domain_rule(head, [list(chain(elem.condition, lits_with_vars))])
         if not self.domain_predicates.has_domain(new_predicate):
-            return [rule]
+            # NOTE: issue #9, this should become an assert
+            return [rule] # nocoverage
 
         # 2. create dom and next_ predicates for it, and then use it to
         # create chain with elem.condition + lits_with_vars
--- tests/test_minmax_aggregates.py
+++ tests/test_minmax_aggregates.py
@@ -790,6 +790,52 @@ __max_0_0_11(P,__PREV) :- __chain__max_0_0_11(P,__PREV); not __chain__max_0_0_11
 __max_0_0_11(P,#inf) :- __min_0__dom___max_0_0_11(X); not __chain__max_0_0_11(P,X); person(P).
 #false :- __max_0_0_11(P,__VAR__max_0_0_11); __VAR__max_0_0_11 > 42.""",
         ),
+        (
+            """
+{person(a);person(b)}.
+{
+skill(a, ("some",1), 3);
+skill(a, ("thing",1), 5);
+skill(a, ("programming",1..10), 10);
+skill(a, ("knitting",1..10), 100);
+skill(b, t("cooking",1..10), 10);
+skill(b, t("knitting",1..10), 1)
+}.
+:- X = #max {V, ID : skill(P, ID, V)} = Y, person(P).
+""",
+            """#program base.
+{ person(a); person(b) }.
+{ skill(a,("some",1),3); skill(a,("thing",1),5); skill(a,("programming",(1..10)),10); skill(a,("knitting",(1..10)),100); skill(b,t("cooking",(1..10)),10); skill(b,t("knitting",(1..10)),1) }.
+__dom_skill(a,("some",1),3).
+__dom_skill(a,("thing",1),5).
+__dom_skill(a,("programming",(1..10)),10).
+__dom_skill(a,("knitting",(1..10)),100).
+__dom_skill(b,t("cooking",(1..10)),10).
+__dom_skill(b,t("knitting",(1..10)),1).
+__dom_person(a).
+__dom_person(b).
+__dom___max_0_0_11(V) :- __dom_skill(P,ID,V); __dom_person(P).
+__min_0__dom___max_0_0_11(X) :- X = #min { L: __dom___max_0_0_11(L) }.
+__max_0__dom___max_0_0_11(X) :- X = #max { L: __dom___max_0_0_11(L) }.
+__next_0__dom___max_0_0_11(P,N) :- __min_0__dom___max_0_0_11(P); __dom___max_0_0_11(N); N > P; not __dom___max_0_0_11(B): __dom___max_0_0_11(B), P < B < N.
+__next_0__dom___max_0_0_11(P,N) :- __next_0__dom___max_0_0_11(_,P); __dom___max_0_0_11(N); N > P; not __dom___max_0_0_11(B): __dom___max_0_0_11(B), P < B < N.
+__chain__max_0_0_11(P,V) :- skill(P,ID,V); person(P).
+__chain__max_0_0_11(P,__PREV) :- __chain__max_0_0_11(P,__NEXT); __next_0__dom___max_0_0_11(__PREV,__NEXT).
+__max_0_0_11(P,__PREV) :- __chain__max_0_0_11(P,__PREV); not __chain__max_0_0_11(P,__NEXT): __next_0__dom___max_0_0_11(__PREV,__NEXT).
+__max_0_0_11(P,#inf) :- __min_0__dom___max_0_0_11(X); not __chain__max_0_0_11(P,X); person(P).
+#false :- __max_0_0_11(P,X); X = Y.""",
+        ),
+        (
+            """
+{person(a);person(b)}.
+c(X) :- X = #sum{Y : person(Y)}.
+:- 42 < #max {X : c(X)}, person(P).
+""",
+            """#program base.
+{ person(a); person(b) }.
+c(X) :- X = #sum { Y: person(Y) }.
+#false :- 42 < #max { X: c(X) }; person(P).""",
+        ),
     ],
 )
 def test_minmax_aggregates(prg, converted_prg):
@@ -801,3 +847,13 @@ def test_minmax_aggregates(prg, converted_prg):
     mma = MinMaxAggregator(rdp, dp)
     output = "\n".join(map(str, mma.execute(ast)))
     assert converted_prg == output
+
+
+
+def test_translation_mapping():
+    trans = MinMaxAggregator.Translation(("a", 3), ("b", 4), (2,1,0))
+    assert trans.translate_parameters(["X","Y","Z"]) == ["Z","Y","X"]
+    trans = MinMaxAggregator.Translation(("a", 3), ("b", 3), (2,1))
+    assert trans.translate_parameters(["X","Y","Z"]) == [None,"Y","X"]
+    trans = MinMaxAggregator.Translation(("a", 3), ("b", 3), (None,2,1))
+    assert trans.translate_parameters(["X","Y","Z"]) == [None,"Z","Y"]
