60d22b781ef0c4adf1a6bcc1f6f5d40f085488b4
Max Ostrowski <max.ostrowski@potassco.com>
2023-08-24 13:04:18 +0200

    add logging and cleaned parser

--- src/ngo/__main__.py
+++ src/ngo/__main__.py
@@ -2,9 +2,7 @@
 The main entry point for the application.
 """
 
-from argparse import Action, ArgumentParser, ArgumentTypeError, Namespace
 from itertools import chain
-from typing import Any, Optional
 
 from clingo.ast import AST, parse_files
 
@@ -14,64 +12,9 @@ from ngo.dependency import DomainPredicates, PositivePredicateDependency, RuleDe
 from ngo.literal_duplication import LiteralDuplicationTranslator
 from ngo.minmax_aggregates import MinMaxAggregator
 from ngo.symmetry import SymmetryTranslator
-from ngo.utils.ast import Predicate
 from ngo.utils.globals import UniqueNames
 from ngo.utils.logger import singleton_factory_logger
-from ngo.utils.parser import get_parser
-
-OPTIONS = ["equalities", "summinmax_chains", "symmetry", "duplication", "cleanup"]
-
-
-class PredicateList(Action):
-    """
-    parse a list of input predicates or call auto detection
-    """
-
-    def __call__(
-        self, parser: ArgumentParser, namespace: Namespace, values: Any, option_string: Optional[str] = None
-    ) -> None:
-        if values == "auto":
-            setattr(namespace, self.dest, values)
-        elif values is None or values == "":
-            setattr(namespace, self.dest, [])
-        else:
-            pred_list: list[Predicate] = []
-            predicate_strings = values.split(",")
-            for predicate_string in predicate_strings:
-                sl = predicate_string.split("/")
-                if len(sl) != 2:
-                    raise ArgumentTypeError(
-                        f"Predicates have to be of the form name/arity, but got {predicate_string}."
-                    )
-                name = sl[0].strip(" ")
-                try:
-                    arity = int(sl[1])
-                except ValueError as esc:
-                    raise ArgumentTypeError(f"Arity of predicate {name} should be integer, but got {sl[1]}.") from esc
-                pred_list.append(Predicate(name, arity))
-            setattr(namespace, self.dest, pred_list)
-
-
-class VerifyEnable(Action):
-    """
-    verify and extend enable option set
-    """
-
-    def __call__(
-        self, parser: ArgumentParser, namespace: Namespace, values: Any, option_string: Optional[str] = None
-    ) -> None:
-        if len(values) > 1 and "none" in values:
-            raise ArgumentTypeError("'none' may not be combined with other options.")
-        if "all" in values:
-            values = OPTIONS
-
-        setattr(namespace, self.dest, values)
-
-
-def consistency_check(args: Namespace) -> None:
-    """check for argument consistency and raise ArgumentTypeError in case of inconsistency"""
-    if "cleanup" in args.enable and args.input_predicates is None:
-        raise ArgumentTypeError("Trait `cleanup` needs the option --input-predicates to be set.")
+from ngo.utils.parser import consistency_check, get_parser
 
 
 def main() -> None:
@@ -80,24 +23,9 @@ def main() -> None:
     """
 
     parser = get_parser()
-    parser.add_argument(
-        "--enable",
-        action=VerifyEnable,
-        type=str.lower,
-        nargs="+",
-        choices=["all", "none"] + OPTIONS,
-        default=OPTIONS,
-        help="enables a set of traits",
-    )
-    parser.add_argument(
-        "--input-predicates",
-        action=PredicateList,
-        nargs="?",
-        type=str,
-        help="enter all input predicates in the form 'name/arity', like 'edge/2' as a comma seperated list.",
-    )
     args = parser.parse_args()
     consistency_check(args)
+
     log = singleton_factory_logger("main", args.log)
 
     prg: list[AST] = []
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,14 @@
 Test cases for main application functionality.
 """
 import logging
+from argparse import ArgumentTypeError
 from io import StringIO
 
+import pytest
+
+from ngo.utils.ast import Predicate
 from ngo.utils.logger import singleton_factory_logger
-from ngo.utils.parser import get_parser
+from ngo.utils.parser import OPTIONS, consistency_check, get_parser
 
 
 def test_logger() -> None:
@@ -25,5 +29,32 @@ def test_parser() -> None:
     Test the parser.
     """
     parser = get_parser()
-    ret = parser.parse_args(["--log", "error"])
-    assert ret.log == logging.ERROR
+    args = parser.parse_args(["--log", "error"])
+    assert args.log == logging.ERROR
+    with pytest.raises(ArgumentTypeError) as excinfo:
+        consistency_check(args)
+    assert str(excinfo.value) == "Trait `cleanup` needs the option --input-predicates to be set."
+    args = parser.parse_args(["--enable", "summinmax_chains", "equalities"])
+    assert "equalities" in args.enable
+    assert "summinmax_chains" in args.enable
+    args = parser.parse_args(["--enable", "all"])
+    assert OPTIONS == args.enable
+
+    with pytest.raises(ArgumentTypeError) as excinfo:
+        parser.parse_args(["--enable", "none", "equalities"])
+    assert str(excinfo.value) == "'none' may not be combined with other options."
+    args = parser.parse_args(["--input-predicates", "zero/0, another/14"])
+    assert Predicate("zero", 0) in args.input_predicates
+    assert Predicate("another", 14) in args.input_predicates
+    args = parser.parse_args(["--input-predicates", "auto"])
+    assert args.input_predicates == "auto"
+    args = parser.parse_args(["--input-predicates", ""])
+    assert len(args.input_predicates) == 0
+    args = parser.parse_args(["--input-predicates"])
+    assert len(args.input_predicates) == 0
+    with pytest.raises(ArgumentTypeError) as excinfo:
+        parser.parse_args(["--input-predicates", "zero"])
+    assert str(excinfo.value) == "Predicates have to be of the form name/arity, but got zero."
+    with pytest.raises(ArgumentTypeError) as excinfo:
+        parser.parse_args(["--input-predicates", "zero/one"])
+    assert str(excinfo.value) == "Arity of predicate zero should be integer, but got one."
